392409039385be440ceb42c913244a3b1ba505f8
HyD3c <dlscjf151@naver.com>
2022-11-06 19:56:35 +0900

    feat: transaction 데이터 저장

--- daemon/models/block.js
+++ daemon/models/block.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose');
 const blockSchema = new mongoose.Schema({
         version: {type: Number, required: true},
         timestamp: {type: Number, required: true},
-        height: {type: Number, required: true},
+        height: {type: Number, required: true, index:true},
         previousBlockID: {type: String, required: true},
         transactionRoot: {type: String, required: true},
         generatorPublicKey: {type: String, required: true},
         reward: {type: String, required: true},
         signature: {type: String, required: true},
-        id: {type: String, required: true},
+        id: {type: String, required: true, unique: true},
         maxHeightPreviouslyForged: {type: Number, required: true},
         maxHeightPrevoted: {type: Number, required: true},
         seedReveal: {type: String, required: true},
-        payload: {type: [], required: true},
+        payload: {type: [String], required: true},
     },
     {
         timestamps: true
--- daemon/subscribe.js
+++ daemon/subscribe.js
@@ -11,18 +11,23 @@ mongoose
         subscribeBlocks(async (data) => {
             const decodedBlock = await decodeBlock(data.block)
             const blockData = reStructBlockData(decodedBlock)
+            decodedBlock.payload.map(payload => {
+                Transaction.create(reStructTransactionData(payload, decodedBlock))
+            })
             Block.create(blockData)
         })
         const lastStoredBlockHeight = await Block.getLastStoredBlockHeight()
         const {lastBlockHeight} = await getLastBlock()
         for (let i = lastStoredBlockHeight + 1; i < lastBlockHeight; i++) {
             getBlockByHeight(i).then((block) => {
+                block.payload.map(payload => {
+                    Transaction.create(reStructTransactionData(payload, block))
+                })
                 Block.create(reStructBlockData(block))
             })
         }
     })
     .catch(e => console.error(e));
-getLastBlock().then(console.log)
 
 
 function reStructBlockData(decodedBlock) {
@@ -30,13 +35,32 @@ function reStructBlockData(decodedBlock) {
     Object.assign(blockData, decodedBlock.header)
     Object.assign(blockData, decodedBlock.header.asset)
     delete blockData.asset
-    blockData.payload = decodedBlock.payload
     blockData.previousBlockID = blockData.previousBlockID.toString("hex")
     blockData.transactionRoot = blockData.transactionRoot.toString("hex")
     blockData.generatorPublicKey = blockData.generatorPublicKey.toString("hex")
     blockData.signature = blockData.signature.toString("hex")
     blockData.id = blockData.id.toString("hex")
     blockData.seedReveal = blockData.seedReveal.toString("hex")
+    blockData.payload = decodedBlock.payload.map(payload => {
+        return payload.id.toString("hex")
+    })
     console.log(blockData)
     return blockData
+}
+
+function reStructTransactionData(transaction, block) {
+    let transactionData = {}
+    Object.assign(transactionData, transaction)
+    Object.assign(transactionData, transaction.asset)
+    delete transactionData.asset
+    transactionData.senderPublicKey = transactionData.senderPublicKey.toString("hex")
+    transactionData.recipientAddress = transactionData.recipientAddress.toString("hex")
+    transactionData.id = transactionData.id.toString("hex")
+    transactionData.signatures = transactionData.signatures.map(signature => {
+        return signature.toString("hex")
+    })
+    transactionData.blockID = block.header.id.toString("hex")
+    transactionData.blockHeight = block.header.height
+    console.log(transactionData)
+    return transactionData
 }
