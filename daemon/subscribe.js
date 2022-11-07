45501f26c0f4a600ee433ad6b388cad2915c0fa8
HyD3c <dlscjf151@naver.com>
2022-11-07 22:49:20 +0900

    fix: genesis block 저장 데이터 수정

--- daemon/subscribe.js
+++ daemon/subscribe.js
@@ -17,8 +17,17 @@ mongoose
             })
             Block.create(blockData)
         })
-        const lastStoredBlockHeight = await Block.getLastStoredBlockHeight()
+        let lastStoredBlockHeight = await Block.getLastStoredBlockHeight()
         const {lastBlockHeight} = await getLastBlock()
+        if (lastStoredBlockHeight === -1) {
+            getBlockByHeight(0).then(block => {
+                block.payload.map(payload => {
+                    Transaction.create(reStructTransactionData(payload, block))
+                })
+                Block.create(reStructGenesisBlock(block))
+            })
+            lastStoredBlockHeight = 0;
+        }
         for (let i = lastStoredBlockHeight + 1; i < lastBlockHeight; i++) {
             getBlockByHeight(i).then((block) => {
                 block.payload.map(payload => {
@@ -30,6 +39,22 @@ mongoose
     })
     .catch(e => console.error(e));
 
+function reStructGenesisBlock(decodedBlock) {
+    let blockData = {}
+    Object.assign(blockData, decodedBlock.header)
+    Object.assign(blockData, decodedBlock.header.asset)
+    delete blockData.asset
+    blockData.previousBlockID = blockData.previousBlockID.toString("hex")
+    blockData.transactionRoot = blockData.transactionRoot.toString("hex")
+    blockData.generatorPublicKey = blockData.generatorPublicKey.toString("hex")
+    blockData.signature = blockData.signature.toString("hex")
+    blockData.id = blockData.id.toString("hex")
+    blockData.payload = decodedBlock.payload.map(payload => {
+        return payload.id.toString("hex")
+    })
+    console.log(blockData)
+    return blockData
+}
 
 function reStructBlockData(decodedBlock) {
     let blockData = {}
