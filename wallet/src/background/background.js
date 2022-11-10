b82c614733bec18b4dbaa51d43d0da88a50f1425
HyD3c <dlscjf151@naver.com>
2022-11-10 22:50:04 +0900

    fix: content_security_policy 문제 해결

--- wallet/src/background/background.js
+++ wallet/src/background/background.js
@@ -1,9 +1,8 @@
-chrome.alarms.create({
-  periodInMinutes: 1 / 60,
-});
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get("mnemonic", (res) => {
-    console.log(res.mnemonic);
-  });
-});
+console.log("1")
+const passphrase = require('@liskhq/lisk-passphrase')
+const transactions = require("@liskhq/lisk-transactions")
+const {apiClient} = require("@liskhq/lisk-client")
+apiClient.createWSClient("ws://34.125.144.144:9000/ws").then(client=>{
+    client.account.get("2ab00e25cc7f4c4764da1c97b0eefd13550c30d4").then(console.log)
+})
+console.log("1")
--- wallet/webpack.js
+++ wallet/webpack.js
@@ -1,80 +1,82 @@
 const path = require("path");
-const { ProvidePlugin } = require("webpack");
+const {ProvidePlugin} = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    popup: path.resolve("src/index.jsx"),
-    background: path.resolve("src/background/background.js"),
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js|\.jsx$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".jsx", ".js"],
-    fallback: {
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify"),
-      url: require.resolve("url"),
+    mode: "production",
+    entry: {
+        popup: path.resolve("src/index.jsx"),
+        background: path.resolve("src/background/background.js"),
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve("public"),
-          to: path.resolve("build"),
+    module: {
+        rules: [
+            {
+                loader: "babel-loader",
+                test: /\.js|\.jsx$/,
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".jsx", ".js"],
+        fallback: {
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("stream-browserify"),
+            assert: require.resolve("assert"),
+            http: require.resolve("stream-http"),
+            https: require.resolve("https-browserify"),
+            os: require.resolve("os-browserify"),
+            url: require.resolve("url"),
+            path: require.resolve("path-browserify"),
+            fs: false,
+            net: false
         },
-      ],
-    }),
-    new ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
-    }),
-    ...getHtmlPlugins(["popup"]),
-  ],
-  output: {
-    filename: "[name].js",
-    path: path.resolve("build"),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
-  },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve("public"),
+                    to: path.resolve("build"),
+                },
+            ],
+        }),
+        new ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+        ...getHtmlPlugins(["popup"]),
+    ],
+    output: {
+        filename: "[name].js",
+        path: path.resolve("build"),
+    },
+    optimization: {
+        concatenateModules: true,
+    },
 };
 
 function getHtmlPlugins(chunks) {
-  return chunks.map(
-    (chunk) =>
-      new HtmlPlugin({
-        title: "React Extension",
-        filename: `${chunk}.html`,
-        chunks: [chunk],
-      })
-  );
+    return chunks.map(
+        (chunk) =>
+            new HtmlPlugin({
+                title: "React Extension",
+                filename: `${chunk}.html`,
+                chunks: [chunk],
+            })
+    );
 }
