033c4d9fd5e25f5e420f6269375adb24f2194898
Dohu Jeon <adc0612@gmail.com>
2022-11-12 13:30:38 +0900

    style: Home 주소창 정렬

--- wallet/src/pages/Home.jsx
+++ wallet/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-chrome-extension-router";
-import Button from "@mui/material/Button";
-import Layout from "../components/layout/Layout";
-import Transaction from "./Transaction";
-import { getAccount } from "../utils/storage";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import { transactions } from "@liskhq/lisk-client";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-chrome-extension-router';
+import Button from '@mui/material/Button';
+import Layout from '../components/layout/Layout';
+import Transaction from './Transaction';
+import { getAccount } from '../utils/storage';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { transactions } from '@liskhq/lisk-client';
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
 
 const Home = () => {
   const [account, changeAccount] = useState({});
@@ -18,56 +20,26 @@ const Home = () => {
   }, []);
   return (
     <Layout>
-      <div style={{ textAlign: "center" }}>
-        <div style={{ marginTop: "20px" }}>
-          <span
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span
-              style={{
-                fontWeight: "bolder",
-                fontSize: "16px",
-                marginLeft: "20px",
-              }}
-            >
-              Account
-            </span>
-            <div
-              style={{
-                cursor: "pointer",
-                fontSize: "16px",
-                marginRight: "20px",
-                lineHeight: "16px",
-                verticalAlign: "middle",
-              }}
-            >
-              {account.address
-                ? account.address.slice(0, 8) +
-                  "..." +
-                  account.address.slice(-7)
-                : ""}
-              <ContentCopyIcon onClick={() => copyInvoice(invoice)} />
-            </div>
-          </span>
-          <hr style={{ marginTop: "20px", backgroundColor: "#dada" }} />
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ marginTop: '20px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Typography sx={{ fontWeight: 'bolder', fontSize: '16px', marginLeft: '20px' }}>Account</Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer', fontSize: '16px', marginRight: '20px' }}>
+              {account.address ? account.address.slice(0, 8) + '...' + account.address.slice(-7) : ''}
+              <ContentCopyIcon sx={{ fontSize: '16px', marginLeft: '8px' }} onClick={() => copyInvoice(invoice)} />
+            </Box>
+          </Box>
+          <hr style={{ marginTop: '20px', backgroundColor: '#dada' }} />
         </div>
-        <div style={{ marginTop: "45px" }}>
-          <img width="50px" height="50px" src="lisk.png" alt="lisk gas" />
+        <div style={{ marginTop: '45px' }}>
+          <img width='50px' height='50px' src='lisk.png' alt='lisk gas' />
           <h2>LSK</h2>
-          <h3>
-            {account.balance
-              ? transactions.convertBeddowsToLSK(account.balance).toString()
-              : "0"}{" "}
-            LSK
-          </h3>
+          <h3>{account.balance ? transactions.convertBeddowsToLSK(account.balance).toString() : '0'} LSK</h3>
         </div>
       </div>
-      <div style={{ textAlign: "center", marginTop: "40px" }}>
-        <Link component={Transaction} style={{ textDecoration: "none" }}>
-          <Button variant="contained">전송</Button>
+      <div style={{ textAlign: 'center', marginTop: '40px' }}>
+        <Link component={Transaction} style={{ textDecoration: 'none' }}>
+          <Button variant='contained'>전송</Button>
         </Link>
       </div>
     </Layout>
